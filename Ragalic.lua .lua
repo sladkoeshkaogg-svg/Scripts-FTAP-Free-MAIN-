51440bbcb8c2c6b3102060bbfa07de667163178b
sladkoeshkaogg-svg <sladkoeshkaogg@gmail.com>
2026-07-22 21:39:42 +0500

    Update Ragalic.lua .lua

--- Ragalic.lua .lua
+++ Ragalic.lua .lua
@@ -3479,7 +3479,13 @@ AurasGroup:AddDropdown("DualKickAuraRadius", {
 		"20",
 		"30",
 		"40",
-		"50"
+		"50",
+		"100",
+		"250",
+		"500",
+		"1000",
+		"5000",
+		"10000"
 	},
 	Default = "20",
 	Callback = function(v)
